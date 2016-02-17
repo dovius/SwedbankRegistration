00c436fd97a720001fa36fed1e235cc5f3cf223b
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2016-02-17 21:33:55 +0200

    Updated Index look, style for lang, removed duplicated code

--- src/main/resources/static/js/languageController.js
+++ src/main/resources/static/js/languageController.js
@@ -3,36 +3,35 @@
  */
 var language = localStorage.getItem("LANG");
 
+var header = document.getElementById("headerContactUs")
+var contactUs = document.getElementById("txtRegistration");
+var writeUs = document.getElementById("txtWriteUs");
+var registrationInformation = document.getElementById("txtRegistrationInformation");
+var liLT = document.getElementById("navLT");
+var liEN = document.getElementById("navEN");
+
 if(language === "ENG"){
     changeToEng();
-    changeToEngRegistrationForm();
 } else{
     changeToLt();
 }
 
 function changeToEng(){
-    var header = document.getElementById("headerContactUs");
-    var contactUs = document.getElementById("txtRegistration");
-    var writeUs = document.getElementById("txtWriteUs");
-    var registrationInformation = document.getElementById("txtRegistrationInformation");
-
     header.innerHTML = "Contact Us";
     contactUs.innerHTML = "Register for consultation";
     writeUs.innerHTML = "Write us";
     registrationInformation.innerHTML = "Registration overview";
     localStorage.setItem("LANG", "ENG");
+    liEN.className = "activeNav";
+    liLT.className = "";
 }
 
 function changeToLt(){
-    var header = document.getElementById("headerContactUs")
-    var contactUs = document.getElementById("txtRegistration");
-    var writeUs = document.getElementById("txtWriteUs");
-    var registrationInformation = document.getElementById("txtRegistrationInformation");
-
     header.innerHTML = "Susisiekite su mumis";
     contactUs.innerHTML = "Registruokitės konsultacijai internetu";
     writeUs.innerHTML = "Parašykite mums";
     registrationInformation.innerHTML = "Registracijų peržiūra";
-
+    liEN.className = "";
+    liLT.className = "activeNav";
     localStorage.setItem("LANG", "LT");
 }
