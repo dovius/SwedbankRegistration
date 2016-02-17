6b2e29abe2d4b0e82ec580d25358b3d044a4816f
Vytautas Sugintas <vytautas.sugintas@gmail.com>
2016-02-17 14:36:38 +0200

    Added Multilanguage for index

---
+++ src/main/resources/static/js/languageController.js
@@ -0,0 +1,45 @@
+/**
+ * Created by vytautassugintas on 17/02/16.
+ */
+
+    /*
+     <p><a id="txtRegistration" href="NewRegistration.html">Registruokitės konsultacijai internetu</a></p>
+     <p><a id="txtWriteUs" href="ContactUs.html">Parašykite mums</a></p>
+     <p><a id="txtRegistrationInformation" href="registrationInfo.html">Registracijų informacija</a></p>
+     */
+
+var language = localStorage.getItem("LANG");
+
+if(language === "ENG"){
+    changeToEng();
+} else{
+    changeToLt();
+}
+
+function changeToEng(){
+    var header = document.getElementById("headerContactUs");
+    var contactUs = document.getElementById("txtRegistration");
+    var writeUs = document.getElementById("txtWriteUs");
+    var registrationInformation = document.getElementById("txtRegistrationInformation");
+
+    header.innerHTML = "Contact Us";
+    contactUs.innerHTML = "Register for consultation";
+    writeUs.innerHTML = "Write us";
+    registrationInformation.innerHTML = "Registration information";
+
+    localStorage.setItem("LANG", "ENG");
+}
+
+function changeToLt(){
+    var header = document.getElementById("headerContactUs")
+    var contactUs = document.getElementById("txtRegistration");
+    var writeUs = document.getElementById("txtWriteUs");
+    var registrationInformation = document.getElementById("txtRegistrationInformation");
+
+    header.innerHTML = "Susisiekite su mumis";
+    contactUs.innerHTML = "Registruokitės konsultacijai internetu";
+    writeUs.innerHTML = "Parašykite mums";
+    registrationInformation.innerHTML = "Registracijų informacija";
+
+    localStorage.setItem("LANG", "LT");
+}
